59a19f80055384d190dd8877640b7bbf86063113
TarcisioRodrigues <tarcisiorodrigues454@gmail.com>
2023-09-17 18:47:04 -0300

    terminando register-user

--- frontend/src/app/(pages)/register-user/page.tsx
+++ frontend/src/app/(pages)/register-user/page.tsx
@@ -1,5 +1,5 @@
 import { Banner } from "@/app/components/Banner";
-
+import { FaWhatsapp, FaInstagram, FaTwitter } from "react-icons/fa";
 export default function RegisterUser() {
   return (
     <div className="items-center justify-center w-full">
@@ -8,7 +8,7 @@ export default function RegisterUser() {
       <h1 className="w-full text-center text-indigo-950 text-[112px] font-normal font-['Playfair Display'] flex items-center justify-center">
         Garanta sua vaga
       </h1>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 gap-14">
         <div className="flex flex-col p-4 gap-3">
           <input
             type="text"
@@ -31,7 +31,7 @@ export default function RegisterUser() {
             </button>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center ml-20 ">
           <div className="w-[191.15px] h-[31.94px] text-black text-[32px] font-bold font-['Montserrat'] leading-[38.40px]">
             Contact
           </div>
@@ -43,6 +43,11 @@ export default function RegisterUser() {
           </div>
           <div className="w-[350.68px] h-[63.88px] text-black text-[32px] font-normal font-['Montserrat'] leading-[38.40px]">
             São Luís, Maranhão, Brasil
+          </div>
+          <div className="flex gap-3 items-center text-center">
+            <FaWhatsapp size={40} />
+            <FaInstagram size={40} />
+            <FaTwitter size={40} />
           </div>
         </div>
       </div>
